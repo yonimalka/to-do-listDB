41263838082140c9edd8f7c65de193058ae8aa59
yonimalka <125285965+yonimalka@users.noreply.github.com>
2023-04-02 16:22:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get("/", function(req, res) {
         })
       res.redirect("/");
     } else {
-      res.render("list", {listTitle: "Today", newListItems: data});
+      res.render("List", {listTitle: "Today", newListItems: data});
     }
    
    })
